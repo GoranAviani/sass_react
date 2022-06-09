147862a34dc2a7d6ed92a1585669a26002e64a7e
goranaviani <goranaviani@gmail.com>
2022-06-09 19:43:32 +0200

    sass react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,16 @@ import '../styles/Navbar.css'
 const Navbar = () => {
 
     return (
-        <div className="navbar">
-            <a>Home</a>
-            <a>Help</a>
-            <a>Login</a>
-            <button>nav button</button>
-        </div>
+        <>
+            <div className="navbar">
+                <a>Home</a>
+                <a>Help</a>
+                <a>Login</a>
+                <button>nav button</button>
+            </div>
+            <button>nav button2</button>
+        </>
+
     )
 
 }
